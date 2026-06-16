01664fbef6b69798088ae75d2b2f4f3bbd89c107
shaikh abdul wahhab <adshaikh837@gmail.com>
2026-06-16 11:37:52 +0530

    Remove commented-out code and unused variable in seed.ts; refactor AdminPage to use direct searchParams

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -5,12 +5,11 @@ import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 export const dynamic = 'force-dynamic';
-const AdminPage =  async({
+const AdminPage = ({
   searchParams,
 }: {
-  searchParams:Promise<{ [key: string]: string | undefined }>;
+  searchParams: { [keys: string]: string | undefined };
 }) => {
-  const resolvedSearchParams = await searchParams;
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -40,7 +39,7 @@ const AdminPage =  async({
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendarContainer  searchParams={resolvedSearchParams}/>
+        <EventCalendarContainer searchParams={searchParams}/>
         <Announcements />
       </div>
     </div>
